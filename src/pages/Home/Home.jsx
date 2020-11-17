ddaf3e66bbe966c547b9257841601455a85b3328
Wilton Oliveira <oliveirajrwilton@gmail.com>
2020-11-17 17:56:10 -0300

    Initial commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import './style.css';
+
+import logoImg from '../../assets/logo.svg';
+
+function Home() {
+  return(
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Acompanhe em tempo real os casos de COVID-19 ao redor do mundo</h1>
+        </main>
+
+        <Link to="/app" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/routes.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home/Home';
+
+
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        
+
+        
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
