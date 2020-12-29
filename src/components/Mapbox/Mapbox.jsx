bd7d715876b06e96b5e5701156583ce9bc431d97
Wilton Oliveira <oliveirajrwilton@gmail.com>
2020-12-28 21:59:07 -0300

    Mapbox com o Popup de todos os paises do dataset

--- src/components/Mapbox/Mapbox.jsx
+++ src/components/Mapbox/Mapbox.jsx
@@ -1,19 +1,20 @@
 import React, {useState} from 'react'
 import ReactMapGL, {Marker} from "react-map-gl"
 import * as countriesCenter from '../../dataset/world_data.json'
-import CovidMarker from "../../assets/marker.svg"
+import Popup from '../../components/Popup/Popup'
 import "./style.css"
 
 
 export default function Mapbox(){
     const [view, setView] = useState({
-        latitude: 0,
+        latitude: 15,
         longitude: 0,
         width: "100vw",
         height: "100vh",
-        zoom: 2
+        zoom: 2.5
     })
 
+    if(view.zoom >= 3){
     return(
         <div>
            <ReactMapGL 
@@ -31,11 +32,33 @@ export default function Mapbox(){
                 longitude={pais.Longitude}
                 >
                     <div className="marker">
-                        <img src={CovidMarker} alt="Covid Marker"/>
+                       
+                            
+                        <Popup 
+                            lat={view.latitude}
+                            long={view.longitude}
+                            id={pais.postal}
+                        />
                     </div>
                 </Marker>
             ))}
            </ReactMapGL>
         </div>
     )
+    } else if(view.zoom){
+        return(
+            <div>
+               <ReactMapGL 
+               {...view}
+               //mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+               mapboxApiAccessToken="pk.eyJ1Ijoid2lsdG9ub2xpdmVpcmEiLCJhIjoiY2tpa21kOXZqMGI2czJzcnh0ZW5jcXZ5cCJ9.H6fVYOkE2OXAluV4YLQwKw"
+               mapStyle="mapbox://styles/wiltonoliveira/ckj8rtre41ydp19ph25d8ckhc"
+               onViewportChange={view => {
+                   setView(view)
+               }}
+               >
+               </ReactMapGL>
+            </div>
+        )
+    }
 }
